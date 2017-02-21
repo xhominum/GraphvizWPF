181f35e3723510e6d1796b7d8ab109c2e857c414
Simon Tanguay <simontanguay@hotmail.com>
2017-02-21 15:39:15 -0500

    Color added.

--- Graphviz4net/src/Graphviz4Net.Core/Dot/AntlrParser/Generated/DotGrammarLexer.cs
+++ Graphviz4net/src/Graphviz4Net.Core/Dot/AntlrParser/Generated/DotGrammarLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// $ANTLR 3.3.1.7705 ./Dot/AntlrParser/DotGrammar.g 2017-02-20 22:57:04
+// $ANTLR 3.3.1.7705 ./Dot/AntlrParser/DotGrammar.g 2017-02-21 15:33:10
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 219
--- Graphviz4net/src/Graphviz4Net.WPF.Example/MainWindowViewModel.cs
+++ Graphviz4net/src/Graphviz4Net.WPF.Example/MainWindowViewModel.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Input;
+using PostSharp.Samples.CustomLogging.Helpers;
 
 namespace Graphviz4Net.WPF.Example
 {
     using Graphs;
     using System.ComponentModel;
+    using System.Windows.Media;
 
     public class Person : INotifyPropertyChanged, IVertice
     {
@@ -21,16 +23,60 @@ namespace Graphviz4Net.WPF.Example
 
     	private string name;
     	public string VerticeName
-    	{
+        {
     		get { return this.name; }
     		set
     		{
     			this.name = value;
     			if (this.PropertyChanged != null) {
-    				this.PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+    				this.PropertyChanged(this, new PropertyChangedEventArgs("VerticeName"));
     			}
     		}
     	}
+
+        SolidColorBrush mySolidColorBrush = new SolidColorBrush(Color.FromArgb(255, 255, 255, 0));
+
+        private Brush itemColor;
+        public Brush ItemColor
+        {
+            get { return itemColor; }
+            set
+            {
+                this.itemColor = value;
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("ItemColor"));
+                }
+            }
+        }
+
+        private string className;
+        public string ClassName
+        {
+            get { return this.className; }
+            set
+            {
+                this.className = value;
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("ClassName"));
+                }
+            }
+        }
+
+        private string methodName;
+        public string MethodName
+        {
+            get { return this.methodName; }
+            set
+            {
+                this.methodName = value;
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("MethodName"));
+                }
+            }
+        }
 
         private string comment;
         public string Comment
@@ -47,13 +93,6 @@ namespace Graphviz4Net.WPF.Example
 
         public string Avatar { get; set; }
 
-        public string Email
-        {
-            get
-            {
-                return this.VerticeName.ToLower().Replace(' ', '.') + "@gmail.com";
-            }
-        }
 
         public ICommand AddCommand
         {
@@ -125,66 +164,141 @@ namespace Graphviz4Net.WPF.Example
     {        
     }
 
+    public class VertexItem
+    {
+        public string VerticeName { get; set; }
+        public Person p;
+    }
+
+    public class SubGraphItem
+    {
+        public string ClassName { get; set; }
+        public SubGraph<IVertice> _subGraph;
+        public Color _color;
+    }
+
     public class MainWindowViewModel : INotifyPropertyChanged
     {
         public MainWindowViewModel()
         {
             var graph = new Graph<IVertice>();
-            var a = new Person(graph) { VerticeName = "Jonh", Comment = "Test", Avatar = "./Avatars/avatar1.jpg" };
-            var b = new Person(graph) { VerticeName = "Michael", Avatar = "./Avatars/avatar2.gif" };
-            var c = new Person(graph) { VerticeName = "Kenny" };
-            var d = new Person(graph) { VerticeName = "Lisa" };
-            var e = new Person(graph) { VerticeName = "Lucy", Avatar = "./Avatars/avatar3.jpg" };
-            var f = new Person(graph) { VerticeName = "Ted Mosby" };
-            var g = new Person(graph) { VerticeName = "Glen" };
-            var h = new Person(graph) { VerticeName = "Alice", Avatar = "./Avatars/avatar1.jpg" };
-
-            var un = new Person(graph) { VerticeName = "AAAAAAAAAAAA"};
-
-            graph.AddVertex(a);
-            graph.AddVertex(b);
-            graph.AddVertex(c);
-            graph.AddVertex(d);
-            graph.AddVertex(e);
-            graph.AddVertex(f);
-            graph.AddVertex(un);
-
-            var subGraph = new SubGraph<IVertice> { Label = "Work" };
-            graph.AddSubGraph(subGraph);
-            subGraph.AddVertex(g);
-            subGraph.AddVertex(h);
-            graph.AddEdge(new Edge<IVertice>(g, h));
-            graph.AddEdge(new Edge<IVertice>(a, g));
-
-            var subGraph5 = new SubGraph<IVertice> { Label = "Testtest" };
-            graph.AddSubGraph(subGraph5);
-            var loner5 = new Person(graph) { VerticeName = "Loner5", Avatar = "./Avatars/avatar1.jpg" };
-            subGraph5.AddVertex(loner5);
 
 
-            var subGraph2 = new SubGraph<IVertice> {Label = "School"};
-            graph.AddSubGraph(subGraph2);
-            var loner = new Person(graph) { VerticeName = "Loner", Avatar = "./Avatars/avatar1.jpg" };
-            subGraph2.AddVertex(loner);
-            var loner2 = new Person(graph) { VerticeName = "Loneddddddddr2", Avatar = "./Avatars/avatar1.jpg" };
-            subGraph2.AddVertex(loner2);
-            graph.AddEdge(new Edge<SubGraph<IVertice>>(subGraph, subGraph2) { Label = "Link between groups" } );
+            var allItems = Database.Instance.FindAll();
 
-            graph.AddEdge(new Edge<IVertice>(c, d) { Label = "In love", DestinationArrowLabel = "boyfriend", SourceArrowLabel = "girlfriend" });
+            var uniqueItems = allItems.Select(e => new { Name = e.Name, ClassName = e.ClassName }).Distinct();
+            var uniqueItems2 = allItems.Select(e => new { Name = e.ParentMethod, ClassName = e.ParentClass }).Distinct();
 
-            graph.AddEdge(new Edge<IVertice>(c, g, new Arrow(), new Arrow()));
-            graph.AddEdge(new Edge<IVertice>(c, a, new Arrow()) { Label = "Boss" });
-            graph.AddEdge(new Edge<IVertice>(d, h, new DiamondArrow(), new DiamondArrow()));
-            graph.AddEdge(new Edge<IVertice>(f, h, new DiamondArrow(), new DiamondArrow()));
-            graph.AddEdge(new Edge<IVertice>(f, loner, new DiamondArrow(), new DiamondArrow()));
-            graph.AddEdge(new Edge<IVertice>(f, b, new DiamondArrow(), new DiamondArrow()));
-            graph.AddEdge(new Edge<IVertice>(e, g, new Arrow(), new Arrow()) { Label = "Siblings" });
+            var union1 = uniqueItems.Concat(uniqueItems2).Distinct();
+
+            var listClassNames = union1.Select(x => x.ClassName).Distinct();
+
+            List<SubGraphItem> subGraphItems = new List<SubGraphItem>();
+
+
+            Random rnd = new Random();
+            foreach (var className in listClassNames)
+            {
+                var subGraph = new SubGraph<IVertice> { Label = className };
+                //graph.AddSubGraph(subGraph);
+                var color = Color.FromArgb((byte)rnd.Next(255), (byte)rnd.Next(255), (byte)rnd.Next(255), (byte)rnd.Next(255));
+                subGraphItems.Add(new SubGraphItem { ClassName = className, _subGraph = subGraph, _color = color });
+            }
+
+            List<VertexItem> vItems = new List<VertexItem>();
+
+            foreach(var item in union1)
+            {
+                var verticeName = item.ClassName + ":" + item.Name;
+                var color = subGraphItems.First(x => x.ClassName.Equals(item.ClassName))._color;
+                SolidColorBrush solid = new SolidColorBrush(color);
+                var el = new Person(graph) { VerticeName = verticeName, ClassName = item.ClassName, MethodName = item.Name, ItemColor = solid };
+                var vi = new VertexItem { VerticeName = verticeName, p = el };
+                vItems.Add(vi);
+                
+                var sub = subGraphItems.First(x => x.ClassName.Equals(item.ClassName));
+                //sub._subGraph.AddVertex(el);
+                graph.AddVertex(el);
+            }
+
+            foreach (var item in union1)
+            {
+                try
+                { 
+                    var verticeName = item.ClassName + ":" + item.Name;
+                    var src = vItems.First(x => x.VerticeName.Equals(verticeName));
+                    var dest = allItems.First(x => x.ClassName.Equals(item.ClassName) && x.Name.Equals(item.Name));
+                    var destVersticeName = dest.ParentClass + ":" + dest.ParentMethod;
+                    var dest2 = vItems.First(x => x.VerticeName.Equals(destVersticeName));
+                    graph.AddEdge(new Edge<IVertice>(dest2.p, src.p));
+                 }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.Write("bug");
+                }
+            }
+
+            
+
+
+
+            //var a = new Person(graph) { VerticeName = "Jonh", Comment = "Test", Avatar = "./Avatars/avatar1.jpg" };
+            //var b = new Person(graph) { VerticeName = "Michael", Avatar = "./Avatars/avatar2.gif" };
+            //var c = new Person(graph) { VerticeName = "Kenny" };
+            //var d = new Person(graph) { VerticeName = "Lisa" };
+            //var e = new Person(graph) { VerticeName = "Lucy", Avatar = "./Avatars/avatar3.jpg" };
+            //var f = new Person(graph) { VerticeName = "Ted Mosby" };
+            //var g = new Person(graph) { VerticeName = "Glen" };
+            //var h = new Person(graph) { VerticeName = "Alice", Avatar = "./Avatars/avatar1.jpg" };
+
+            //var un = new Person(graph) { VerticeName = "AAAAAAAAAAAA"};
+
+            //graph.AddVertex(a);
+            //graph.AddVertex(b);
+            //graph.AddVertex(c);
+            //graph.AddVertex(d);
+            //graph.AddVertex(e);
+            //graph.AddVertex(f);
+            //graph.AddVertex(un);
+
+            //var subGraph = new SubGraph<IVertice> { Label = "Work" };
+            //graph.AddSubGraph(subGraph);
+            //subGraph.AddVertex(g);
+            //subGraph.AddVertex(h);
+            //graph.AddEdge(new Edge<IVertice>(g, h));
+            //graph.AddEdge(new Edge<IVertice>(a, g));
+
+            //var subGraph5 = new SubGraph<IVertice> { Label = "Testtest" };
+            //graph.AddSubGraph(subGraph5);
+            //var loner5 = new Person(graph) { VerticeName = "Loner5", Avatar = "./Avatars/avatar1.jpg" };
+            //subGraph5.AddVertex(loner5);
+
+
+            //var subGraph2 = new SubGraph<IVertice> {Label = "School"};
+            //graph.AddSubGraph(subGraph2);
+            //var loner = new Person(graph) { VerticeName = "Loner", Avatar = "./Avatars/avatar1.jpg" };
+            //subGraph2.AddVertex(loner);
+            //var loner2 = new Person(graph) { VerticeName = "Loneddddddddr2", Avatar = "./Avatars/avatar1.jpg" };
+            //subGraph2.AddVertex(loner2);
+            //graph.AddEdge(new Edge<SubGraph<IVertice>>(subGraph, subGraph2) { Label = "Link between groups" } );
+
+            //graph.AddEdge(new Edge<IVertice>(c, d) { Label = "In love", DestinationArrowLabel = "boyfriend", SourceArrowLabel = "girlfriend" });
+
+            //graph.AddEdge(new Edge<IVertice>(c, g, new Arrow(), new Arrow()));
+            //graph.AddEdge(new Edge<IVertice>(c, a, new Arrow()) { Label = "Boss" });
+            //graph.AddEdge(new Edge<IVertice>(d, h, new DiamondArrow(), new DiamondArrow()));
+            //graph.AddEdge(new Edge<IVertice>(f, h, new DiamondArrow(), new DiamondArrow()));
+            //graph.AddEdge(new Edge<IVertice>(f, loner, new DiamondArrow(), new DiamondArrow()));
+            //graph.AddEdge(new Edge<IVertice>(f, b, new DiamondArrow(), new DiamondArrow()));
+            //graph.AddEdge(new Edge<IVertice>(e, g, new Arrow(), new Arrow()) { Label = "Siblings" });
 
             this.Graph = graph;
             this.Graph.Changed += GraphChanged;
             this.NewPersonName = "Enter new name";
         	this.UpdatePersonNewName = "Enter new name";
         }
+
+        
 
         public Graph<IVertice> Graph { get; private set; }
 
@@ -281,5 +395,65 @@ namespace Graphviz4Net.WPF.Example
         {
             return this.Graph.AllVertices.First(x => string.CompareOrdinal(x.VerticeName, name) == 0);
         }
+
+
+        //private void TestCode()
+        //{
+        //    var graph = new Graph<IVertice>();
+        //    var a = new Person(graph) { VerticeName = "Jonh", Comment = "Test", Avatar = "./Avatars/avatar1.jpg" };
+        //    var b = new Person(graph) { VerticeName = "Michael", Avatar = "./Avatars/avatar2.gif" };
+        //    var c = new Person(graph) { VerticeName = "Kenny" };
+        //    var d = new Person(graph) { VerticeName = "Lisa" };
+        //    var e = new Person(graph) { VerticeName = "Lucy", Avatar = "./Avatars/avatar3.jpg" };
+        //    var f = new Person(graph) { VerticeName = "Ted Mosby" };
+        //    var g = new Person(graph) { VerticeName = "Glen" };
+        //    var h = new Person(graph) { VerticeName = "Alice", Avatar = "./Avatars/avatar1.jpg" };
+
+        //    var un = new Person(graph) { VerticeName = "AAAAAAAAAAAA" };
+
+        //    graph.AddVertex(a);
+        //    graph.AddVertex(b);
+        //    graph.AddVertex(c);
+        //    graph.AddVertex(d);
+        //    graph.AddVertex(e);
+        //    graph.AddVertex(f);
+        //    graph.AddVertex(un);
+
+        //    var subGraph = new SubGraph<IVertice> { Label = "Work" };
+        //    graph.AddSubGraph(subGraph);
+        //    subGraph.AddVertex(g);
+        //    subGraph.AddVertex(h);
+        //    graph.AddEdge(new Edge<IVertice>(g, h));
+        //    graph.AddEdge(new Edge<IVertice>(a, g));
+
+        //    var subGraph5 = new SubGraph<IVertice> { Label = "Testtest" };
+        //    graph.AddSubGraph(subGraph5);
+        //    var loner5 = new Person(graph) { VerticeName = "Loner5", Avatar = "./Avatars/avatar1.jpg" };
+        //    subGraph5.AddVertex(loner5);
+
+
+        //    var subGraph2 = new SubGraph<IVertice> { Label = "School" };
+        //    graph.AddSubGraph(subGraph2);
+        //    var loner = new Person(graph) { VerticeName = "Loner", Avatar = "./Avatars/avatar1.jpg" };
+        //    subGraph2.AddVertex(loner);
+        //    var loner2 = new Person(graph) { VerticeName = "Loneddddddddr2", Avatar = "./Avatars/avatar1.jpg" };
+        //    subGraph2.AddVertex(loner2);
+        //    graph.AddEdge(new Edge<SubGraph<IVertice>>(subGraph, subGraph2) { Label = "Link between groups" });
+
+        //    graph.AddEdge(new Edge<IVertice>(c, d) { Label = "In love", DestinationArrowLabel = "boyfriend", SourceArrowLabel = "girlfriend" });
+
+        //    graph.AddEdge(new Edge<IVertice>(c, g, new Arrow(), new Arrow()));
+        //    graph.AddEdge(new Edge<IVertice>(c, a, new Arrow()) { Label = "Boss" });
+        //    graph.AddEdge(new Edge<IVertice>(d, h, new DiamondArrow(), new DiamondArrow()));
+        //    graph.AddEdge(new Edge<IVertice>(f, h, new DiamondArrow(), new DiamondArrow()));
+        //    graph.AddEdge(new Edge<IVertice>(f, loner, new DiamondArrow(), new DiamondArrow()));
+        //    graph.AddEdge(new Edge<IVertice>(f, b, new DiamondArrow(), new DiamondArrow()));
+        //    graph.AddEdge(new Edge<IVertice>(e, g, new Arrow(), new Arrow()) { Label = "Siblings" });
+
+        //    this.Graph = graph;
+        //    this.Graph.Changed += GraphChanged;
+        //    this.NewPersonName = "Enter new name";
+        //    this.UpdatePersonNewName = "Enter new name";
+        //}
     }
 }
